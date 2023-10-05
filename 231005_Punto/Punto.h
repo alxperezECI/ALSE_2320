9f6db32acb4743f046ffed5c0c63497e4cd6443e
JUANITO264 <juan.rruiz@mail.escuelaing.edu.co>
2023-10-05 15:48:44 -0500

    Adding Puntos

---
+++ 231005_Punto/Punto.h
@@ -0,0 +1,16 @@
+#include<iostream>
+
+class Punto{
+    double _x,_y;
+    public:
+    Punto();
+    Punto(double x, double y);
+    double X();
+    double Y();
+    double distancia(Punto &b);
+    void X(const double &x);
+    void Y(const double &y);
+    friend std::ostream& operator<<(std::ostream &out, const Punto &p);
+    Punto operator+(const Punto &b)const;
+    Punto operator/(const double &b)const;
+};
