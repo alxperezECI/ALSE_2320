75dfabe57d2454cb7e6b3b9be3532217bd8ba03c
santiago cortes tovar <scortua@hotmail.com>
2023-10-17 14:54:21 -0500

    Actualizacion de ejercicio

--- 231005_Punto/punto.h
+++ 231005_Punto/punto.h
@@ -1,6 +1,7 @@
 #include <iostream>
 
-class Punto{
+class Punto
+{
   double _x, _y;
 
 public:
@@ -11,5 +12,9 @@ public:
   double Y();
   void X(const double &x);
   void Y(const double &y);
-  friend std::ostream& operator<<(std::ostream &out, const Punto &p);
+  friend std::ostream &operator<<(std::ostream &out, const Punto &p);
+  Punto operator+(const Punto &b);
+  Punto operator/(const double &b);
+  Punto operator+=(const Punto &b);
+  Punto operator/=(const double &b);
 };
