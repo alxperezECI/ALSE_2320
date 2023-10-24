1cec8b40b9c7b59cfb077481b208d868f2f49a4b
santiago cortes tovar <scortua@hotmail.com>
2023-10-24 15:20:36 -0500

    comentarios de main

--- herencia/fig_geometricas_file/main.cpp
+++ herencia/fig_geometricas_file/main.cpp
@@ -12,6 +12,10 @@ int main(int argc, char **argv)
 {
     vector<Geometrica *> vFig;
     Geometrica *pG = nullptr;
+    /*
+    creo un vector de punteros para guardar puntero de objetos que se vayan creando
+    uso un puntero de Geometrica para entender el codigo
+    */
 
     if (argc != 2)
     {
@@ -36,6 +40,7 @@ int main(int argc, char **argv)
 
         while (getline(archivo, path))
         {
+            // en el path tengo el texto que se extrae de datos.txt
             posicion = path.find(" ", posicion);
             // cout << path << "-> " << posicion <<endl;
             tipo = atoi(path.substr(0, posicion).c_str());
@@ -60,11 +65,19 @@ int main(int argc, char **argv)
         }
         cout << vFig.size() << endl;
         archivo.close();
+        /*
+        ese puntero lo agrega a la lista geometrica
+        */
     }
 
     // Procesar el vector
     for (int i = 0; i < vFig.size(); ++i)
     {
+        /*
+        -> miembro de un puntero, le indico que llame a la funcion area a donde apunta el puntero
+        *() desreferencia el puntero, le indico que llame a la funcion area del objeto al que apunta el puntero
+
+        */
         cout << "El área es: " << vFig[i]->area() << endl;
         cout << *(vFig[i]) << endl;
         cout << vFig[i]->getTipo() << endl;
