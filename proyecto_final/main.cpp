5a75b5f1ae155a227f60ba2caf717f7512992dea
david-bolivar-ECI <david.bolivar-a@mail.escuelaing.edu.co>
2023-12-01 14:37:47 -0500

    update periodo

--- proyecto_final/main.cpp
+++ proyecto_final/main.cpp
@@ -15,37 +15,52 @@ int main(int argc, char **argv)
 
     while (true)
     {
-        dr_viento Sensorviento;
-        humedad Sensorhumedad;
-        int_luz Sensorluz;
-        precipitacion Sensorprecipitacion;
-        temperatura Sensortemperatura;
-        velocidad Sensorvelocidad;
+        int pathfr = stoi(argv[2]);
+        if (argc != 3)
+        {
+            cout << "No se ingresó un nombre de base de datos o periodo de toma de datos" << endl;
+            return 1;
+        }
+        if (pathfr < 5 || pathfr > 20)
+        {
+            cout << "El periodo de toma de datos debe ser mayor a 5 segundos y menor a 20 segundos" << endl;
+            return 1;
+        }
+        else
+        {
+            dr_viento Sensorviento;
+            humedad Sensorhumedad;
+            int_luz Sensorluz;
+            precipitacion Sensorprecipitacion;
+            temperatura Sensortemperatura;
+            velocidad Sensorvelocidad;
 
-        int datoviento = Sensorviento.generarDato();
-        cout << "Dato generado por dr_viento: " << datoviento << "° Norte" << endl;
+            int datoviento = Sensorviento.generarDato();
+            cout << "Dato generado por dr_viento: " << datoviento << "° Norte" << endl;
 
-        int datohumedad = Sensorhumedad.generarDato();
-        cout << "Dato generado por humedad: " << datohumedad << " %" << endl;
+            int datohumedad = Sensorhumedad.generarDato();
+            cout << "Dato generado por humedad: " << datohumedad << " %" << endl;
 
-        int datoluz = Sensorluz.generarDato();
-        cout << "Dato generado por int_luz: " << datoluz << " Lumen" << endl;
+            int datoluz = Sensorluz.generarDato();
+            cout << "Dato generado por int_luz: " << datoluz << " Lumen" << endl;
 
-        int datoprecipitacion = Sensorprecipitacion.generarDato();
-        cout << "Dato generado por precipitacion: " << datoprecipitacion << " mm" << endl;
+            int datoprecipitacion = Sensorprecipitacion.generarDato();
+            cout << "Dato generado por precipitacion: " << datoprecipitacion << " mm" << endl;
 
-        int datotemperatura = Sensortemperatura.generarDato();
-        cout << "Dato generado por temperatura: " << datotemperatura << "° C" << endl;
+            int datotemperatura = Sensortemperatura.generarDato();
+            cout << "Dato generado por temperatura: " << datotemperatura << "° C" << endl;
 
-        int datovelocidad = Sensorvelocidad.generarDato();
-        cout << "Dato generado por velocidad: " << datovelocidad << " m/s" << endl;
+            int datovelocidad = Sensorvelocidad.generarDato();
+            cout << "Dato generado por velocidad: " << datovelocidad << " m/s" << endl;
 
-        string dbPath(argv[1]);
-        DBSensores dbSensores(dbPath);
-        cout << "Tomando datos de los sensores" << endl;
-        dbSensores.insert(datoviento, datohumedad, datoluz, datoprecipitacion, datotemperatura, datovelocidad);
+            string dbPath(argv[1]);
 
-        this_thread::sleep_for(chrono::seconds(5));
+            DBSensores dbSensores(dbPath);
+            cout << "Tomando datos de los sensores" << endl;
+            dbSensores.insert(datoviento, datohumedad, datoluz, datoprecipitacion, datotemperatura, datovelocidad);
+
+            this_thread::sleep_for(chrono::seconds(pathfr));
+        }
     }
 
     return 0;
